cdfb3519b8b8a6264f37350567e2100ba3181773
bobcoi03 <elongatedmusk6969@gmail.com>
2022-04-26 00:14:11 +0100

    changed application.py

--- application.py
+++ application.py
@@ -27,7 +27,6 @@ import main_site
 application.register_blueprint(main_site.bp)
 
 if __name__ == '__main__':
-	application.debug = True
-	application.run()
+	application.run(debug=True,host='0.0.0.0', port=5000)
 
 	
